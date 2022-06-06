e2b173ecd165f5f07536b9d1471f248eeabdbe27
Sarah_Arya092 <sarah.1997@live.be>
2022-06-07 01:20:37 +0200

    version mobile ok

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,9 @@
-console.info('Hello world');
+"use strict";
+
+import { gsap } from 'gsap';
+
+const tl = gsap.timeline();
+
+      tl.from('.discover', { opacity: 0, scale: 1, duration: 0.8 });
+
+      tl.to('.discover', { opacity: 1, scale: 2, duration: 0.8 });
